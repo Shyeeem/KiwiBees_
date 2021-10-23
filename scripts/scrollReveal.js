5e065083fa2bebbdf0a28ef3c5556fabfb884103
Shyeeem <48987868+Shyeeem@users.noreply.github.com>
2021-10-23 13:26:20 +1100

    Initial Site Upload

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,29 @@
+/*HEADER SCRIPTS*/
+ScrollReveal().reveal('header h1', {delay: 25});
+ScrollReveal().reveal('header h2', {delay: 25});
+ScrollReveal().reveal('header h3', {delay: 25});
+ScrollReveal().reveal('header hr', {delay: 25});
+ScrollReveal().reveal('header p', {delay: 25});
+
+/*PERSONAL PROJECTS SECTION SCRIPTS*/
+ScrollReveal().reveal('#personal h2', {delay: 25});
+ScrollReveal().reveal('#personal p', {delay: 25});
+ScrollReveal().reveal('#outsiders h3', {delay: 25});
+ScrollReveal().reveal('#outsiders p', {delay: 25});
+ScrollReveal().reveal('#outsiders figure', {delay: 25});
+ScrollReveal().reveal('#lovebites h3', {delay: 25});
+ScrollReveal().reveal('#lovebites p', {delay: 25});
+ScrollReveal().reveal('#lovebites figure', {delay: 25});
+
+/*SHORT STORIES SECTION SCRIPTS*/
+ScrollReveal().reveal('#shorts h2', {delay: 25});
+ScrollReveal().reveal('#shorts p', {delay: 25});
+ScrollReveal().reveal('#shorts h3', {delay: 25});
+ScrollReveal().reveal('#row1', {delay: 75});
+ScrollReveal().reveal('#row2', {delay: 75});
+
+/*FOOTER SCRIPTS*/
+ScrollReveal().reveal('footer h6', {delay: 25});
+ScrollReveal().reveal('footer p', {delay: 25});
+ScrollReveal().reveal('footer a', {delay: 25});
+ScrollReveal().reveal('#socials', {delay: 25});
